84ff30a08a7fd0f0f86fd0238517c2c0e774a8d8
Igggosha <igggoshastudios@gmail.com>
2023-07-23 09:20:36 +0100

    Expanded VillagerInventory slightly
    only a small helper function really

--- Assets/Scripts/Villanger/VillagerInventory.cs
+++ Assets/Scripts/Villanger/VillagerInventory.cs
@@ -1,15 +1,35 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+
 namespace Villanger
 {
     public class VillagerInventory
     {
 
-        private readonly int numberOfSlots;
-        
+        public readonly int numberOfSlots = 10;
+        public int slotsTaken;
+        public List<string> inventory;
+
         public VillagerInventory(int numberOfSlots)
         {
             this.numberOfSlots = numberOfSlots;
+
         }
+        
 
-
+        /// <summary>
+        /// Small function to add an item to inventory safely.
+        /// </summary>
+        /// <param name="space">The amount of space the item takes up</param>
+        /// <param name="name">The name of the item</param>
+        public void AddItemToInventory(int space, string name)
+        {
+            if (slotsTaken + space <= numberOfSlots)
+            {
+                inventory.Add(name);
+                slotsTaken += space;
+            }
+        }
     }
 }
